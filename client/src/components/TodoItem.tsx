acb46e2fffd8bb7c6ae4467416bf62f8f17ea1d1
kamalisreesathiyamoorthi <kamalisree11@gmail.com>
2025-05-26 20:37:08 +0530

    BASE_URL bug fixed

--- client/src/components/TodoItem.tsx
+++ client/src/components/TodoItem.tsx
@@ -1,24 +1,120 @@
+// import { Badge, Box, Flex, Spinner, Text, useQuery } from "@chakra-ui/react";
+// import { FaCheckCircle } from "react-icons/fa";
+// import { MdDelete } from "react-icons/md";
+// import type { Todo } from "./TodoList";
+// import { useMutation, useQueryClient } from "@tanstack/react-query";
+// import { BASE_URL } from "../App";
+
+// const TodoItem = ({ todo }: { todo: Todo }) => {
+// 	const queryClient = useQueryClient() ;
+// 	const {mutate:updateTodo,isPending:isUpdating} = useMutation({
+// 		mutationKey:["updateTodo"] ,
+// 		mutationFn:async()=> {
+// 			if(todo.completed) return alert("Todo is already completed!")
+// 			try {
+// 				const res = await fetch(BASE_URL + `/todos/${todo.id}` , {
+// 					method : "PATCH" ,
+// 				})
+// 				const data = await res.json()
+// 				if(!res.ok){
+// 					throw new Error(data.error || "Something went wrong!")
+// 				}
+// 				return data
+// 			}
+// 			catch(error){
+// 				console.log(error)
+// 			}
+// 		} ,
+// 		onSuccess : () =>  {
+// 			queryClient.invalidateQueries({queryKey : ["todos"]});
+// 		}
+// 	});
+
+// 	const {mutate:deleteTodo , isPending : isDeleting } = useMutation ({
+// 		mutationKey : ["deleteTodo"] ,
+// 		mutationFn : async () => {
+// 			try {
+// 				const res = await fetch(BASE_URL + `/todos/${todo.id}`, {
+// 					method : "DELETE" ,
+// 				}) ;
+// 				const data = await res.json() ;
+// 				if(!res.ok) {
+// 					throw new Error(data.error || "Something went wrong!");
+// 				}
+// 				return data ;
+// 			}catch(error) {
+// 				console.log(error)
+// 			}
+// 		} ,
+// 		onSuccess : () => {
+// 			queryClient.invalidateQueries({queryKey : ["todos"]}) ;
+// 		},
+
+// 	});
+
+// 	return (
+// 		<Flex gap={2} alignItems={"center"}>
+// 			<Flex
+// 				flex={1}
+// 				alignItems={"center"}
+// 				border={"1px"}
+// 				borderColor={"gray.600"}
+// 				p={2}
+// 				borderRadius={"lg"}
+// 				justifyContent={"space-between"}
+// 			>
+// 				<Text
+// 					color={todo.completed ? "green.200" : "yellow.600"}
+// 					textDecoration={todo.completed ? "line-through" : "none"}
+// 				>
+// 					{todo.body}
+// 				</Text>
+// 				{todo.completed && (
+// 					<Badge ml='1' colorScheme='green'>
+// 						Done
+// 					</Badge>
+// 				)}
+// 				{!todo.completed && (
+// 					<Badge ml='1' colorScheme='yellow'>
+// 						In Progress
+// 					</Badge>
+// 				)}
+// 			</Flex>
+// 			<Flex gap={2} alignItems={"center"}>
+// 				<Box color={"green.500"} cursor={"pointer"} onClick={ () => updateTodo()}>
+// 					{!isUpdating && <FaCheckCircle size = {20} />}
+// 					{isUpdating && <Spinner size = {"sm"} />}
+// 				</Box>
+// 				<Box color={"red.500"} cursor={"pointer"} onClick={ () => deleteTodo()}>
+// 					{!isDeleting && <MdDelete size={25} /> }
+// 					{isDeleting && <Spinner size={"sm"} /> }
+// 				</Box>
+// 			</Flex>
+// 		</Flex>
+// 	);
+// };
+// export default TodoItem;
+
 import { Badge, Box, Flex, Spinner, Text } from "@chakra-ui/react";
 import { FaCheckCircle } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
-import type{ Todo } from "./TodoList";
+import type { Todo } from "./TodoList";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { BASE_URL } from "../App";
 
 const TodoItem = ({ todo }: { todo: Todo }) => {
 	const queryClient = useQueryClient();
-
 	const { mutate: updateTodo, isPending: isUpdating } = useMutation({
 		mutationKey: ["updateTodo"],
 		mutationFn: async () => {
-			if (todo.completed) return alert("Todo is already completed");
+			if (todo.completed) return alert("Todo is already completed!");
 			try {
-				const res = await fetch(BASE_URL + `/todos/${todo._id}`, {
+				const res = await fetch(BASE_URL + `/todos/${todo.id}`, {
 					method: "PATCH",
 				});
 				const data = await res.json();
 				if (!res.ok) {
-					throw new Error(data.error || "Something went wrong");
+					throw new Error(data.error || "Something went wrong!");
 				}
 				return data;
 			} catch (error) {
@@ -34,12 +130,12 @@ const TodoItem = ({ todo }: { todo: Todo }) => {
 		mutationKey: ["deleteTodo"],
 		mutationFn: async () => {
 			try {
-				const res = await fetch(BASE_URL + `/todos/${todo._id}`, {
+				const res = await fetch(BASE_URL + `/todos/${todo.id}`, {
 					method: "DELETE",
 				});
 				const data = await res.json();
 				if (!res.ok) {
-					throw new Error(data.error || "Something went wrong");
+					throw new Error(data.error || "Something went wrong!");
 				}
 				return data;
 			} catch (error) {
@@ -63,7 +159,8 @@ const TodoItem = ({ todo }: { todo: Todo }) => {
 				justifyContent={"space-between"}
 			>
 				<Text
-					color={todo.completed ? "green.200" : "yellow.100"}
+					fontWeight="bold"
+					color={todo.completed ? "#00FF00" : "#FFD700"}
 					textDecoration={todo.completed ? "line-through" : "none"}
 				>
 					{todo.body}
@@ -92,4 +189,5 @@ const TodoItem = ({ todo }: { todo: Todo }) => {
 		</Flex>
 	);
 };
+
 export default TodoItem;
